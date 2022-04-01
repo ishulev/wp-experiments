1ba4e8bd64e4aba91845a1a78fe98dc0f036cb1e
Ivan Shulev <ivanshulev@Ivans-MacBook-Pro.local>
2022-04-01 16:27:02 +0300

    Extract dog functionality to its own class

--- mu-plugins/poslushen-admin/poslushen-admin.php
+++ mu-plugins/poslushen-admin/poslushen-admin.php
@@ -4,5 +4,9 @@ if ( !function_exists( 'add_action' ) ) {
 	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
 	exit;
 }
- 
-add_action( 'init', 'wpdocs_codex_book_init' );
+
+define( 'POSLUSHEN_ADMIN__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+require_once( POSLUSHEN_ADMIN__PLUGIN_DIR . 'class.poslushen-dogs.php' );
+
+add_action( 'init', array( 'Poslushen_Dogs', 'init' ) );
